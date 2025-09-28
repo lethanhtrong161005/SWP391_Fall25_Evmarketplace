13c35e5edc5ddccb27e0f93dd6353ab3ccc326ef
Le Thanh Trong <lethanhtrong161005@gmail.com>
2025-09-28 11:20:46 +0700

    Update SecurityConfig.java
    fix-config

--- src/main/java/com/swp391/evmarketplace/SWP391_Fall25_Evmarketplace/config/SecurityConfig.java
+++ src/main/java/com/swp391/evmarketplace/SWP391_Fall25_Evmarketplace/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
                             "/api/accounts/reset-password",
 
                             "/api/listing/all",
-                            "/api/listing/**"
+                            "/api/listing/**",
 
                             "/api/accounts/image/{fileName:.+}/avatar"
                     ).permitAll();
